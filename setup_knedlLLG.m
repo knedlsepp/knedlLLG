3121ef585b3bbf4c176c1a0c9dbca27db7150730
Josef Kemetmüller <josef.kemetmueller@aon.at>
2014-10-05 16:25:09 +0200

    Added shortest possible Readme

--- setup_knedlLLG.m
+++ setup_knedlLLG.m
@@ -1,6 +1,8 @@
 % THIS SCRIPT COMPILES THE BISECTION-SOURCE
 [knedlpathstrmain, name, ext] = fileparts(mfilename('fullpath'));
 addpath(genpath(knedlpathstrmain),'-begin');
+assert(exist('setup_p1afem_nD', 'file')==2,'Submodule p1afem-nD not found');
+assert(exist('compileKOperator', 'file')==2,'Submodule K-operator not found');
 setup_p1afem_nD;
 compileKOperator;
 cd(knedlpathstrmain);
